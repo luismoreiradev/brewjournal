d1245dad97bac3bb566440ca9358e3f7ab6427e0
luchopatagonico <59893924+luchopatagonico@users.noreply.github.com>
2022-05-31 13:12:05 -0300

    datafotdatabase solucionado , arreglar funcion de conversion de unidades solo cambia en front pero no en data for database

--- src/pages/Calculador.js
+++ src/pages/Calculador.js
@@ -20,92 +20,87 @@ setAllGrains(prevVal=>{return [...prevVal,granoIngresado]})
  
   }
 
+let titulo = allGrains.map(a => a.titulo);
+let nombreGrano = allGrains.map(a => a.nombreGrano);
+let nombreLupulo = allGrains.map(a => a.nombreLupulo);
+let cantidadGrano = allGrains.map(a => a.cantidadGrano);
+let cantidadLupulo = allGrains.map(a => a.cantidadLupulo);
 
-  let nombreGrano = allGrains.map(a => a.nombreGrano);
- let nombreLupulo = allGrains.map(a => a.nombreLupulo);
- let cantidadGrano = allGrains.map(a => a.cantidadGrano);
- let cantidadLupulo = allGrains.map(a => a.cantidadLupulo);
+let indexGrano=0 
+let grain="grano"
+let grano
 
-  let indexGrano=0 
- let grain="grano"
-    let grano
+let indexLupulo=0 
+let hop="lupulo"
+let lupulo
 
-     let indexLupulo=0 
- let hop="lupulo"
-    let lupulo
-    let dataForDataBase={}
+let indexCantidadGrano=0 
+let grainQuantity="cantidadGrano"
+let granoCantidad
 
-  function bla(){ 
-nombreGrano.forEach(element => {   
-  if (element.length > 0) {
-    indexGrano++
-    grano=grain.concat(indexGrano)
-   dataForDataBase[grano]=element   
-    console.log(dataForDataBase);
-  }  
-  else{
-    console.log(" no hay grano");
+let indexCantidadLupulo=0 
+let hopQuantity="cantidadLupulo"
+let lupuloCantidad
+
+let dataForDataBase={}
+
+function setDataForDB(){ 
+    dataForDataBase["titulo"]=titulo[0]
+    
+        nombreGrano.forEach(element => {   
+          if (element.length > 0) {
+            indexGrano++
+            grano=grain.concat(indexGrano)
+          dataForDataBase[grano]=element   
+            
+          }  
+        
+        });
+
+        cantidadGrano.forEach(element => {   
+          if (element.length > 0) {
+            indexCantidadGrano++
+            granoCantidad=grainQuantity.concat(indexCantidadGrano)
+          dataForDataBase[granoCantidad]=element   
+            
+          }  
+          
+        });
+
+        nombreLupulo.forEach(element => {   
+          if (element.length > 0) {
+            indexLupulo++
+            lupulo=hop.concat(indexLupulo)
+          dataForDataBase[lupulo]=element   
+          
+          }  
+        
+        });
+
+        cantidadLupulo.forEach(element => {   
+          if (element.length > 0) {
+            indexCantidadLupulo++
+            lupuloCantidad=hopQuantity.concat(indexCantidadLupulo)
+          dataForDataBase[lupuloCantidad]=element   
+            
+          }  
+        
+        });
   }
-});
 
-nombreLupulo.forEach(element => {   
-  if (element.length > 0) {
-    indexLupulo++
-    lupulo=hop.concat(indexLupulo)
-   dataForDataBase[lupulo]=element   
-    console.log(dataForDataBase);
-  }  
-  else{
-    console.log(" no hay lupulo");
-  }
-});
-
-
-  }
-  bla()
+ 
+  setDataForDB()
 
 
 
 function guardarReceta() {
-     axios.post("http://localhost:3000/recetas", /*  {
-      titulo:allGrains.titulo,
-      grano1:nombreGrano[0],
-      grano2:nombreGrano[1],
-      grano3:nombreGrano[2],
-      grano4:nombreGrano[3],
-      grano5:nombreGrano[4],
-      grano6:nombreGrano[5],
-      grano7:nombreGrano[6],
-      cantidadGrano1:cantidadGrano[0],
-      cantidadGrano2:cantidadGrano[1],
-      cantidadGrano3:cantidadGrano[2],
-      cantidadGrano4:cantidadGrano[3],
-      cantidadGrano5:cantidadGrano[4],
-      cantidadGrano6:cantidadGrano[5],
-      cantidadGrano7:cantidadGrano[6],
-      lupulo1:nombreLupulo[0],
-      lupulo2:nombreLupulo[1],
-      lupulo3:nombreLupulo[2],
-      lupulo4:nombreLupulo[3],
-      lupulo5:nombreLupulo[4],
-      lupulo6:nombreLupulo[5],
-      cantidadLupulo1:cantidadLupulo[0],
-      cantidadLupulo2:cantidadLupulo[1],
-      cantidadLupulo3:cantidadLupulo[2],
-      cantidadLupulo4:cantidadLupulo[3],
-      cantidadLupulo5:cantidadLupulo[4],
-      cantidadLupulo6:cantidadLupulo[5],
-      levadura:allGrains.levadura
-        
-  }*/)
+     axios.post("http://localhost:3000/recetas",
+     dataForDataBase )
    .then(function (response) {
     console.log(response.data,"data guardada");
   })
   }
 
- 
-
-  
   return (    
     <div>
     <h1>Brewery Calculator</h1>
