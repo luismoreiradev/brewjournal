87d1943babc962e3433e0bea51dddc26c91fdaae
luismoreiradev <luismoreiradev@gmail.com>
2023-01-10 16:59:08 -0300

    nuevo reseteo de maquina

--- server/server/bin/mongodb.js
+++ server/server/bin/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/breweryDB', {useNewUrlParser: true,
+mongoose.connect('mongodb://127.0.0.1:27017/breweryDB', {useNewUrlParser: true,
 useUnifiedTopology: true},function (error) {if (error) {throw error;    
 }else {console.log("conexion a mongodb exitosa")}
     
