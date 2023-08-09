c3998e43f65f2748f1367491d45756a2cc3d3544
luismoreiradev <luismoreiradev@gmail.com>
2023-08-09 16:55:32 -0300

    nueva compu editando el boton de edtiar 2

--- src/components/RecetaEnParticular.js
+++ src/components/RecetaEnParticular.js
@@ -1,7 +1,10 @@
 import { useHistory } from "react-router-dom";
+import React, { useState } from "react";
 const axios = require('axios');
 
 function RecetaEnParticular(props) {
+
+  const [identificadorParaUpdate, setIdentificadorParaUpdate] = useState(""); 
 
   const history = useHistory();
   const navigateToRecetas = () => {
@@ -10,11 +13,18 @@ function RecetaEnParticular(props) {
     window.location.reload(false);
     };
 
+    let id
+    
+
     function borrarReceta() {
       console.log(data._id);
       axios.delete("http://localhost:3000/recetas/"+data._id)
       navigateToRecetas()
     }
+
+
+
+
 
     let data = props.datos
     
@@ -24,15 +34,26 @@ function RecetaEnParticular(props) {
       {
       Object.keys(data).map((key, index) => {
         if(key==="_id" || key==="__v"){
+          id =  data._id
+                    
           return(
             <span></span>
           )
-        }else{ return (
+        }else{
+         
+          
+           
+          return (
           <div key={index}>
             <h2>
               {key}: {data[key]}
+             
             </h2>
-
+             
+          <button onClick={() => {
+                  setIdentificadorParaUpdate(key);
+                  console.log(`Button for ${identificadorParaUpdate} clicked.`);
+                }}>editar {key}</button>
           
           </div>
         );}
