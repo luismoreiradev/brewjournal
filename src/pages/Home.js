b2bf12cc913061bfedf0b1ef7cafda9e95083dda
luismoreiradev <luismoreiradev@gmail.com>
2023-11-30 14:30:55 -0300

    nueva copia

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ function Home() {
     return ( 
         <div>
         <h1>Home page</h1>
-        <p>esta pagina esta vacia todavia</p>
+        <p>esta pagina esta vacia todavia pero ya la voy a llenar</p>
         </div>
      );
 }
